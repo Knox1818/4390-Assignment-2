b9d69d1f4c537130b1e5d11fbcf3bfb54c06bf47
Ben Collier <bgcollier1@buffs.wtamu.edu>
2023-03-06 21:33:37 -0600

    Initial

--- Assignment4/Food2UTests.cs
+++ Assignment4/Food2UTests.cs
@@ -1,10 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Xunit;
 using Moq;
-using RazorPagesSample.Pages;
-using RazorPagesSample.Models;
 
-namespace RazorPagesSample.Tests
+namespace Food2U.Tests
 {
     public class CustomerManagementTests
     {
@@ -41,8 +38,8 @@ namespace RazorPagesSample.Tests
             {
                 Input = new LoginModel.InputModel
                 {
-                    Email = "johndoe@example.com",
-                    Password = "P@ssw0rd!"
+                    Email = "heisenberg@gmail.com",
+                    Password = "Walter123"
                 }
             };
 
@@ -64,8 +61,8 @@ namespace RazorPagesSample.Tests
             {
                 Input = new AccountModel.InputModel
                 {
-                    Name = "Jane Doe",
-                    Email = "janedoe@example.com"
+                    Name = "Walter White",
+                    Email = "heisenberg@gmail.com"
                 }
             };
 
@@ -76,6 +73,36 @@ namespace RazorPagesSample.Tests
             Assert.IsType<RedirectToPageResult>(result);
             var redirectResult = (RedirectToPageResult)result;
             Assert.Equal("/Account", redirectResult.PageName);
+        }
+        public class PaymentManagementTests
+        {
+            [Fact]
+            public void Test_ProcessPayment()
+            {
+            // Arrange
+            var cart = new Cart();
+            cart.AddItem(new MenuItem("Cheeseburger", 5.99));
+            var payment = new Payment("Walter White", "1234567890123456", "123", DateTime.Now.AddMonths(1), 50);
+
+            // Act
+            var result = PaymentProcessor.ProcessPayment(cart, payment);
+            
+            // Assert
+            Assert.True(result.Success);
+            Assert.NotNull(result.TransactionId);
+            }
+
+            [Fact]
+            public void Test_ViewSalesHistory()
+            {
+                // Arrange
+                var restaurant = new Restaurant("KFC");
+                var order1 = new Order(DateTime.Now.AddDays(-2), new List<CartItem> { new CartItem(new MenuItem("Fried chicken", 7.99), 2) });
+                var order2 = new Order(DateTime.Now.AddDays(-1), new List<CartItem> { new CartItem(new MenuItem("Mashed potatoes", 3.99), 1), new CartItem(new MenuItem("Coleslaw", 2.99), 1) });
+                order1.Pay(new Payment("Gus Fring", "1234567890123456", "123", DateTime.Now.AddMonths(1), 20));
+                order2.Pay(new Payment("Jesse Pinkman", "9876543210987654", "456", DateTime.Now.AddMonths(2), 10));
+                restaurant.Orders.Add(order1);
+            }
         }
     }
 }
